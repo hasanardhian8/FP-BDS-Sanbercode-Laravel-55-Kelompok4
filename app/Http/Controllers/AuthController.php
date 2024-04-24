7b2a92a837f82a9bbcaa9577992277c25c0f7e97
hasanardhian8 <hasanardhian11@gmail.com>
2024-04-25 00:09:15 +0700

    register & login

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    //
+    function register(){
+        return view('register');
+    }
+    public function registerPost(Request $request)
+    {
+        $user = new User();
+        $user->first_name = $request->first_name;
+        $user->last_name = $request->last_name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->save();
+        Auth::login($user);
+        return redirect('/post')->with('success', 'Register successfully');
+    }
+
+    public function login()
+    {
+        return view('login');
+    }
+
+    public function loginPost(Request $request)
+    {
+        $credetials = [
+            'email' => $request->email,
+            'password' => $request->password,
+        ];
+        if (Auth::attempt($credetials)) {
+            return redirect('/post')->with('success', 'Login berhasil');
+        }
+        return back()->with('error', 'Email or Password salah');
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect()->route('login');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\postController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+    Route::group(['middleware' => 'guest'], function () {
+        Route::get('/register', [AuthController::class, 'register'])->name('register');
+        Route::post('/register', [AuthController::class, 'registerPost'])->name('register');
+        Route::get('/login', [AuthController::class, 'login'])->name('login');
+        Route::post('/login', [AuthController::class, 'loginPost'])->name('login');
+    });
+    Route::group(['middleware' => 'auth'], function () {
+        Route::get('/post', [PostController::class, 'index']);
+        Route::delete('/logout', [AuthController::class, 'logout'])->name('logout');
+    });
