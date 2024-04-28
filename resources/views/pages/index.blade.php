b07b290dd43816097aa758e7a1232b928416b0b8
bimabaktimandala <bmbakti@gmail.com>
2024-04-29 06:22:24 +0700

    laravel ui(auth) dan template

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.master')
+@section('title','Halaman Utama')
+@section('content')
+    <h1>SanberBook</h1>
+    <h2>Social Media Developer Santai Berkualitas</h2>
+    <p>Belajar dan Berbagi agar hidup ini semakin berkualitas</p>
+    <h3>Benefit Join di SanberBook</h3>
+    <ul>
+        <li>Mendapatkan motivasi dari sesama developer</li>
+        <li>Sharing knowledge dari para mastah Sanber</li>
+        <li>Dibuat oleh calon web developer terbaik</li>
+    </ul>
+    <h3>Cara Bergabung ke SanberBook</h3>
+    <ol>
+        <li>Mengunjungi Website ini</li>
+        <li>Mendaftar di <a href="#">Form Sign Up</a></li>
+        <li>Selesai!</li>
+    </ol>
+@endsection
+    
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/h', function () {
+    return view('pages.index');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
